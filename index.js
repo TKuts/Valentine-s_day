605a96c0355e47820980d7915eb72cd93a92f387
Anton Kuts <anton.a.kuts@gmail.com>
2024-02-13 19:20:44 +0200

    completed layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const lidTwo = document.querySelector(".two");
 const latter = document.querySelector(".letter");
 
 function activePage() {
-  wrapper.classList.add("hidden");
+  wrapper.classList.add("show");
 }
 
 function openEnvalope() {
@@ -15,3 +15,22 @@ function openEnvalope() {
 
 window.setTimeout(activePage, 2000);
 wrapper.addEventListener("click", openEnvalope);
+
+function hearts() {
+  const body = document.querySelector("body");
+
+  console.log(body);
+
+  const create = document.createElement("div");
+  create.classList.add("hearts");
+  create.innerHTML = "❤️";
+
+  create.style.left = Math.random() * 100 + "vw";
+  create.style.animationDuration = Math.random() * 2 + 3 + "s";
+  body.appendChild(create);
+  setTimeout(() => {
+    create.remove();
+  }, 3000);
+}
+
+setInterval(hearts, 100);
